80ad48196167ac4c480988a6ee271c4f45494384
visuve <veli-matti.visuri@iki.fi>
2024-12-17 23:00:53 +0200

    Create timer to measure tournament

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,32 @@
 #include "Tournament.hpp"
 
-constexpr size_t Rounds = 200;
+class Timer
+{
+public:
+	Timer() :
+		_start(std::chrono::high_resolution_clock::now())
+	{
+	}
+
+	friend std::ostream& operator << (std::ostream& os, const Timer& timer)
+	{
+		const auto end = std::chrono::high_resolution_clock::now();
+		const auto diff = std::chrono::duration_cast<std::chrono::microseconds>(end - timer._start);
+		return os << "Took: " << diff;
+	}
+
+private:
+	const std::chrono::time_point<std::chrono::high_resolution_clock> _start;
+};
 
 int main()
 {
 	{
+		Timer timer;
+
 		constexpr size_t PlayerCount = 3;
 		constexpr size_t CompetitionCount = PlayerCount * PlayerCount;
-		constexpr size_t RoundsPerCompetition = 5;
+		constexpr size_t RoundsPerCompetition = 200;
 
 		Ast::Player a(new Ast::Strategies::Tit4Tat(), RoundsPerCompetition, CompetitionCount);
 		Ast::Player b(new Ast::Strategies::Friedman(), RoundsPerCompetition, CompetitionCount);
@@ -21,6 +40,8 @@ int main()
 		{
 			++tournament;
 		}
+
+		std::cout << timer << std::endl;
 
 		tournament.PrintMatrix(std::cout);
 
